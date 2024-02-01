27933dd15e9ad6f84fe2a0f141fd0168b5af9199
John Ritter <john.ritter.8788@gmail.com>
2024-02-01 12:54:21 -0500

    Hunted down error that was causing issues, updating with beginnings of tetromino code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     const GRID_WIDTH = 10;
     const GRID_HEIGHT = 20;
     const GRID_SIZE = GRID_WIDTH * GRID_HEIGHT;
-
     const grid = createGrid();
     let squares = Array.from(grid.querySelectorAll('div'));
     const startBtn = document.querySelector('.button');
@@ -11,22 +10,70 @@ document.addEventListener('DOMContentLoaded', () =>{
     const span = document.getElementsByClassName('close')[0];
     const scoreDisplay = document.querySelector('.score-display');
     const linesDisplay = document.querySelector('.lines-score');
-    let currentindex = 0;
+    let currentIndex = 0;
     let currentRotation = 0;
     const width = 10;
     let score = 0;
     let lines = 0;
     let timerId;
     let nextRandom = 0;
-    const color = [
+    const colors = [
         'url(images/blue_block.png)',
         'url(images/pink_block.png)',
         'url(images/purple_block.png)',
         'url(images/peach_block.png)',
         'url(images/yellow_block.png)'
-    ]
+    ];
 
-    function createGrid(){
-        
+    function createGrid() {
+        let grid = document.querySelector('.grid');
+        for (let i = 0; i < GRID_SIZE; i++) {
+            let gridElement = document.createElement('div');
+            grid.appendChild(gridElement);
+        }
+
+        for (let i = 0; i < GRID_WIDTH; i++) {
+            let gridElement = document.createElement('div');
+            gridElement.setAttribute('class', 'block3');
+            grid.appendChild(gridElement);
+        }
+
+        let previousGrid = document.querySelector('.previous-grid');
+        for (let i = 0; i < 16; i++) {
+            let gridElement = document.createElement('div');
+            previousGrid.appendChild(gridElement);
+        }
+        return grid;
     }
+
+    function control(e) {
+        if (e.keyCode === 39)
+            moveright();
+        else if (e.keyCode === 38)
+            rotate();
+        else if (e.keyCode === 37)
+            moveleft();
+        else if (e.keyCode === 40)
+            moveDown();
+    }
+
+    document.addEventListener('keydown', control);
+
+    const lTetromino = [
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, 2],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2],
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2],
+        [GRID_WIDTH, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2 + 2]
+    ];
+
+    const zTetromino = [
+        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
+        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+    ];
+
+    const tTetromino = [
+        
+    ]
 })
